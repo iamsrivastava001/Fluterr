c57505f12e9cbcc3b790416c72c5ca1ca8eeff37
Aman <srivastavaaman9785@gmail.com>
2021-02-25 23:39:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+abc = 122
+bac = 123
+document.write(abc)
+document.write("<br>");
+document.write(bac)
+document.write("<br>");
+document.write(abc + bac)
+y=abc++
+
+document.write("<br>");
+document.write(y)
+document.write("<br>");
+document.write(abc)
+
